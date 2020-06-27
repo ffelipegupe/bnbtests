b832115a651ba8b1ca2a6d73ca4f78a47d5eab63
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-26 21:10:54 -0500

    Edit: styling

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,16 @@ from uuid import uuid4
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
+            datefor = '%Y-%m-%dT%H:%M:%S.%f'
             self.id = kwargs.get('id')
-            ca = datetime.strptime(kwargs.get('created_at'), '%Y-%m-%dT%H:%M:%S.%f')
+            ca = datetime.strptime(kwargs.get('created_at'), datefor)
             self.created_at = ca
-            ua = datetime.strptime(kwargs.get('updated_at'), '%Y-%m-%dT%H:%M:%S.%f')
+            ua = datetime.strptime(kwargs.get('updated_at'), datefor)
             self.updated_at = ua
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
 
     def __str__(self):
         a = type(self).__name__

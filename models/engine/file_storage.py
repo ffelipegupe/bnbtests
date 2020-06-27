4e35dea5388e14d6d37256349e5e1628e2eed712
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-26 21:41:27 -0500

    Edit: function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,9 +25,9 @@ class FileStorage:
         try:
             with open(FileStorage.__file_path) as f:
                 objdic = json.load(f)
-                for value in objdic.values():
-                    cls_name = value["__class__"]
-                    del value["__class__"]
-                    self.new(eval(cls_name)(**value))
+                for o in objdic.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return

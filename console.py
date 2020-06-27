9e67002ca81c8c93ccd969ae04158f708110eb3f
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-26 22:46:23 -0500

    Update: console.py

--- console.py
+++ console.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 import cmd
+import sys
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    __functions = {
+        "BaseModel"
+    }
 
     def do_quit(self, s):
         return True
@@ -16,6 +22,19 @@ class HBNBCommand(cmd.Cmd):
 
     do_EOF = do_quit
     help_EOF = help_quit
+
+    def do_create(self, arg):
+        args = parse(arg)
+        if len(args) == 1:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__functions:
+            print("** class doesn't exist")
+        else:
+            print(eval(args[0]().id))
+            storage.save()
+
+def parse(arg):
+    return tuple(map(str, arg.split()))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
